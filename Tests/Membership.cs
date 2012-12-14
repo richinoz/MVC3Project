a0ee53159768585c1a7fc3eedfebff6c0a225281
unknown <rsmith@it-2433.man-ap.co>
2012-12-14 17:13:40 +1100

    added first test

---
+++ Tests/Membership.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Dynamic;
+using System.Linq;
+using System.Text;
+
+namespace Tests
+{
+    class Membership
+    {
+        public dynamic Register(string password, string email, string userName)
+        {
+            dynamic result = new ExpandoObject();
+            result.Success = false;
+            return result;
+        }
+    }
+}
---
+++ Tests/MembershipTests.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using MVC3ProjectShare.Controllers;
+using MVC3ProjectShare.Models;
+using NUnit.Framework;
+
+namespace Tests
+{
+    [TestFixture]
+    public class MembershipTests
+    {        
+        [Test]
+        public void valid_email_and_password_should_register_successfully()
+        {
+            var membership = new Membership();
+
+            var result = membership.Register("password","test@test.com", "test");
+
+            Assert.False(result.Success);
+            
+        }
+    }
+}
